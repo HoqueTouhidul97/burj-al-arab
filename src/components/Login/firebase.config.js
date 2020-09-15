b40aa890c6c4249550e46288b9337181dd0e6dd7
HoqueTouhidul97 <67569444+HoqueTouhidul97@users.noreply.github.com>
2020-09-15 09:29:48 +0600

    authentication completed

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9YCH6cV_6f-eOxXWNAS3YWB4T8QcTZtM",
+    authDomain: "burj-al-arab-8be11.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-8be11.firebaseio.com",
+    projectId: "burj-al-arab-8be11",
+    storageBucket: "burj-al-arab-8be11.appspot.com",
+    messagingSenderId: "15315137420",
+    appId: "1:15315137420:web:92dce6c5b108ecd4dddca9"
+  };
+  export default firebaseConfig;
